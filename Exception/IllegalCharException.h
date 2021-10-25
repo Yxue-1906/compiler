94af4d505c2a7325ecf6956f7e6fc371dc966d82
nanashi <na@na.shi>
2021-10-25 15:48:31 +0800

    save effort

---
+++ Exception/IllegalCharException.h
@@ -0,0 +1,14 @@
+//
+// Created by unrelated on 2021/10/25.
+//
+
+#ifndef PARSER_ILLEGALCHAREXCEPTION_H
+#define PARSER_ILLEGALCHAREXCEPTION_H
+
+
+class IllegalCharException {
+
+};
+
+
+#endif //PARSER_ILLEGALCHAREXCEPTION_H
